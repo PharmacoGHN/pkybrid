f2c659bd994f7387e05880536c0f55ff88af7b33
Romain Garreau <romain.garreau@chu-lyon.fr>
2024-07-27 16:20:14 +0200

    create model function

---
+++ R/model_input.R
@@ -0,0 +1,76 @@
+#' create_model
+#'
+#' create model is the generic function that allow user to very easily create a new model file
+#'
+#' @param type prespescified type of model.
+#' @param constant specify if the model should be parametrized using micro of macro constant
+#' @param cov is a list containing parameters and covariates relationship and beta value
+#' @param engine nonmem by default. as of version 1.0.0 only nonmem engine is supported
+#'
+#' @export
+
+
+
+create_model <- function(type, constant = c("micro", "macro"), cov = NULL, engine = "parametric") {
+
+  if (!type %in% c("oral_1cp", "oral_2cp", "iv_1cp", "iv_2cp", "scim_1cp", "scim_2cp")) {
+    stop("Invalid model type. Choose from 'oral_1cp', 'oral_2cp', 'iv_1cp', 'iv_2cp', 'scim_1cp', 'scim_2cp'")
+  }
+
+  if (!engine %in% c("parametric", "NPAG")) {
+    stop("Invalid engine. Chose either parametric or NPAG.")
+  }
+
+  if (engine == "NPAG") {
+    stop("NPAG is not available in V1.0")
+  }
+
+  if (!constant %in% c("micro", "macro")) {
+    stop("Invalid constant. Model parameter should be 'macro' (CL, V etc.) or 'micro' (ke, V, etc.).")
+  }
+
+  model_type <- get_model_type(type, constant)
+
+
+  # Temporary placehord until this line can be implemented : model_file <- get_model(model_type, cov, engine)
+  model_file <- "placeholder_model_file"
+
+  return(model_file)
+}
+
+#' get_model_type
+#'
+#' generated the model name to call generic model
+#'
+#' @param type type of model. see create model
+#' @param constant type of constant used. see create model
+#'
+#' @noRd
+
+get_model_type <- function(type, constant, tlag = FALSE) {
+
+  modeltype <- strsplit(type,  split = "_")
+  absorption_model <- ifelse(modeltype[[1]][1] == "iv", "", "_ka")
+
+  parameters <- dplyr::case_when(
+    modeltype[[1]][2] == "1cp" & constant  == "macro" ~ "Cl.V",
+    modeltype[[1]][2] == "1cp" & constant  == "micro" ~ "ke.V",
+    modeltype[[1]][2] == "2cp" & constant  == "macro" ~ "Cl.V.Q.V2",
+    modeltype[[1]][2] == "2cp" & constant  == "micro" ~ "ke.V.k12.k21",
+    .default = NA
+  )
+
+  if (is.na(parameters)) {
+    stop("Non existing model. Please see ?pkybrid::default_model for more information")
+  }
+
+  model_name <- paste0(type, absorption_model, "_", parameters)
+
+
+  if (tlag) {
+    if (modeltype[[1]][1] == "iv") {stop("Can't have a Tlag with an iv model")}
+    model_name <- paste0(model_name, "_Tlag")
+  }
+
+  return(model_name)
+}
---
+++ tests/testthat/test-model_input.R
@@ -0,0 +1,59 @@
+test_that("create_model works as expected", {
+  # Testing valid input
+  expect_equal(create_model("oral_1cp", "micro"), "placeholder_model_file")
+  expect_equal(create_model("oral_2cp", "macro"), "placeholder_model_file")
+  expect_equal(create_model("iv_1cp", "micro"), "placeholder_model_file")
+  expect_equal(create_model("iv_2cp", "macro"), "placeholder_model_file")
+  expect_equal(create_model("scim_1cp", "micro"), "placeholder_model_file")
+  expect_equal(create_model("scim_2cp", "macro"), "placeholder_model_file")
+
+  # Testing invalid model type
+  expect_error(
+    create_model("invalid_type", "micro", engine = "parametric"),
+    "Invalid model type. Choose from 'oral_1cp', 'oral_2cp', 'iv_1cp', 'iv_2cp', 'scim_1cp', 'scim_2cp'"
+  )
+
+  # Testing invalid engine
+  expect_error(
+    create_model("oral_1cp", "micro", engine = "invalid_engine"),
+    "Invalid engine. Chose either parametric or NPAG."
+  )
+
+  # Testing NPAG engine
+  expect_error(
+    create_model("oral_1cp", "micro", engine = "NPAG"),
+    "NPAG is not available in V1.0"
+  )
+
+  # Testing invalid constant
+  expect_error(create_model("oral_1cp", constant = "invalid_constant", engine = "parametric"), "Invalid constant. Model parameter should be 'macro' (CL, V etc.) or 'micro' (ke, V, etc.).", fixed = TRUE)
+})
+
+
+test_that("get_model_type works as expected", {
+
+  # Test 1cp models with macro and micro constants
+  expect_equal(get_model_type("oral_1cp", "macro"), "oral_1cp_ka_Cl.V")
+  expect_equal(get_model_type("oral_1cp", "micro"), "oral_1cp_ka_ke.V")
+  expect_equal(get_model_type("iv_1cp", "macro"), "iv_1cp_Cl.V")
+  expect_equal(get_model_type("iv_1cp", "micro"), "iv_1cp_ke.V")
+  expect_equal(get_model_type("oral_1cp", "macro", tlag = TRUE), "oral_1cp_ka_Cl.V_Tlag")
+
+  # Test 2cp models with macro and micro constants
+  expect_equal(get_model_type("oral_2cp", "macro"), "oral_2cp_ka_Cl.V.Q.V2")
+  expect_equal(get_model_type("oral_2cp", "micro"), "oral_2cp_ka_ke.V.k12.k21")
+  expect_equal(get_model_type("iv_2cp", "macro"), "iv_2cp_Cl.V.Q.V2")
+  expect_equal(get_model_type("iv_2cp", "micro"), "iv_2cp_ke.V.k12.k21")
+
+  # Test sc_im_1cp models with macro and micro constants
+  expect_equal(get_model_type("scim_1cp", "macro"), "scim_1cp_ka_Cl.V")
+  expect_equal(get_model_type("scim_1cp", "micro"), "scim_1cp_ka_ke.V")
+
+  # Test scim_2cp models with macro and micro constants
+  expect_equal(get_model_type("scim_2cp", "macro"), "scim_2cp_ka_Cl.V.Q.V2")
+  expect_equal(get_model_type("scim_2cp", "micro"), "scim_2cp_ka_ke.V.k12.k21")
+
+  # Test invalid model type and constant combinations
+  expect_error(get_model_type("oral_3cp", "macro"), "Non existing model. Please see ?pkybrid::default_model for more information", fixed = TRUE)
+  expect_error(get_model_type("iv_2cp", "macro", tlag = TRUE), "Can't have a Tlag with an iv model")
+})
